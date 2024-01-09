7d9d3d96b6941dc18b02189db11cf23e3d5a4616
Gordon <gordonjackman@gmail.com>
2024-01-09 17:04:13 -0500

    What I worked on this weekend.  Most of server.js is done along with a lot of the routes. Still have to get db.json to be written to.

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js')
+const fs = require('fs');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/404.html'))
+);
+  
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
