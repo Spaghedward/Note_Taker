8648e4eb1ffebb45c3445bb33c45bb2e25f2244e
Gordon <gordonjackman@gmail.com>
2024-01-11 14:11:58 -0500

    The get request works, but the post request doesn't.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,3 +1,42 @@
 const notes = require('express').Router();
+const fs = require('fs');
 
-notes.post()
+
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const list = JSON.parse(data);
+        res.json(list);
+    
+
+})});
+
+notes.post('/', (req, res) => {
+    fs.readFileSync('./db/db.json', 'utf8', (err) => {
+        if (err) throw err;
+    });
+
+    const newNote = req.body;
+    fs.appendFile('./db/db.json', newNote, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.readFileSync('./db/db.json', 'utf8', (data) => {
+                const updatedList = JSON.parse(data);
+                res.json(updatedList);
+            });
+        }
+        
+        
+    });
+
+    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    //     if (err) throw err;
+    //     const list = JSON.parse(data);
+    //     res.json(list);
+    // });
+
+});
+
+module.exports = notes;
